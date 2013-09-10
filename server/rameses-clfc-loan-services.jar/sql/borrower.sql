d9e60571c399555734651e802ce245e4ca54bee4
worgflores <worgflores@gmail.com>
2013-09-10 11:57:44 +0800

    updated the rameses-clfc-loan-services

--- server/rameses-clfc-loan-services.jar/sql/borrower.sql
+++ server/rameses-clfc-loan-services.jar/sql/borrower.sql
@@ -6,3 +6,9 @@ SELECT * FROM borrower_children WHERE borrowerid=$P{borrowerid}
 
 [removeChildren]
 DELETE FROM borrower_children WHERE borrowerid=$P{borrowerid} 
+
+[removeEmployments]
+DELETE FROM employment WHERE refid=$P{refid} 
+
+[removeOtherIncomes]
+DELETE FROM sourceofincome WHERE refid=$P{refid} 
