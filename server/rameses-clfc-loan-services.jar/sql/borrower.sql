0e7dae49037f5bba4ee02d5946726d816fb1bf0d
worgflores <worgflores@gmail.com>
2013-09-10 11:12:14 +0800

    updated the Borrowerservice

--- server/rameses-clfc-loan-services.jar/sql/borrower.sql
+++ server/rameses-clfc-loan-services.jar/sql/borrower.sql
@@ -1,2 +1,8 @@
 [findByObjid]
-SELECT objid FROM borrower WHERE objid=$P{objid}
+SELECT objid FROM borrower WHERE objid=$P{objid} 
+
+[getChildren]
+SELECT * FROM borrower_children WHERE borrowerid=$P{borrowerid} 
+
+[removeChildren]
+DELETE FROM borrower_children WHERE borrowerid=$P{borrowerid} 
