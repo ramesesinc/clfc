3c66a6b2f99665f13e31dbfb9e1d8159606365b0
worgflores <worgflores@gmail.com>
2013-09-16 11:51:59 +0800

    updated the rameses-clfc-customer

---
+++ client/rameses-clfc-customer/src/com/rameses/clfc/customer/CustomerSearchController.groovy
@@ -0,0 +1,62 @@
+package com.rameses.clfc.customer;
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+
+class CustomerSearchController extends BasicLookupModel 
+{
+    //to be feed by the caller
+    
+    @Service('CustomerService')
+    def service;
+    
+    void init() { 
+        
+    } 
+
+    boolean show(String searchtext) { 
+        customerlistHandler.searchtext = searchtext; 
+        return true; 
+    }
+    
+    def getTitle() {
+        return ''' 
+            <font color="#808080" size="5"><b>Search Customers</b></font><br>
+        '''; 
+    } 
+    
+    def getValue() { 
+        return customerlistHandler.selectedValue; 
+    } 
+    
+    def selectedCustomer; 
+    def customerlistHandler = [ 
+        fetchList: {o-> 
+            return service.getList(o);  
+        } 
+    ] as PageListModel;
+
+    void search() { 
+        customerlistHandler.reload(); 
+    } 
+    
+    void moveFirstPage() {  
+        customerlistHandler.moveFirstPage(); 
+    } 
+    
+    void moveBackPage() { 
+        customerlistHandler.moveBackPage(); 
+    } 
+    
+    void moveNextPage() {  
+        customerlistHandler.moveNextPage(); 
+    } 
+    
+    void moveLastPage() {}     
+    
+    void view() {
+        
+    }
+} 
