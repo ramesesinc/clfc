0b5729ae912bce7ee0e5c71dcea96618f89a7087
worgflores <worgflores@gmail.com>
2013-09-16 13:22:21 +0800

    updated the Borrowerservice

--- client/rameses-clfc-loan/src/com/rameses/clfc/loan/controller/NewLoanAppController.groovy
+++ client/rameses-clfc-loan/src/com/rameses/clfc/loan/controller/NewLoanAppController.groovy
@@ -47,7 +47,7 @@ class NewLoanApplicationController
         return 'Loan Application: ' + entity.mode;
     }
     
-    def customerLookupHandler = InvokerUtil.lookupOpener('customer:lookup', [:]); 
+    def customerLookupHandler = InvokerUtil.lookupOpener('customer:search', [:]); 
     
     def save() {
         if (entity.clienttype == 'MARKETED') {
