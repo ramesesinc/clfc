d56b27e45b091fe4411ee51347307c365679a830
worgflores <worgflores@gmail.com>
2013-09-26 19:22:25 +0800

    updated the shared services

--- osiris3-server/workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/collector.sql
+++ osiris3-server/workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/collector.sql
@@ -15,6 +15,13 @@ FROM sys_usergroup_member ug
 WHERE ug.user_objid = $P{userid}
 AND usergroupid = 'COLLECTOR'
 
+[findUserTxnCode]
+SELECT ug.usertxncode 
+FROM sys_usergroup_member ug 
+WHERE ug.user_objid = $P{userid}
+AND usergroupid = 'COLLECTOR'
+
+
 [findCollector]
 SELECT 
 user_objid as objid, 
@@ -24,4 +31,15 @@ user_firstname as firstname,
 jobtitle as title
 FROM sys_usergroup_member 
 WHERE user_objid = $P{userid}
-AND usergroupid = 'COLLECTOR'
+AND usergroupid = 'COLLECTOR' 
+
+[findSubCollector]
+SELECT 
+user_objid as objid, 
+user_username as username, 
+user_lastname as lastname,
+user_firstname as firstname,
+jobtitle as title
+FROM sys_usergroup_member 
+WHERE user_objid = $P{userid}
+AND usergroupid = 'SUBCOLLECTOR' 
