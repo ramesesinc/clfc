1d9ce10a3c6c6ecc1952027c141312249ac9ac1c
worgflores <worgflores@gmail.com>
2013-09-27 16:07:45 +0800

    updated the rameses-clfc-customer-services

--- server/rameses-clfc-customer-services.jar/sql/customer.sql
+++ server/rameses-clfc-customer-services.jar/sql/customer.sql
@@ -4,3 +4,11 @@ WHERE ${filter} ORDER BY name
 
 [findCustomer]
 SELECT * FROM customer WHERE objid=$P{objid} 
+
+[getConnections]
+SELECT 
+	r.objid, r.name, r.lastname, r.firstname, r.middlename, c.relationship
+FROM customer_connection c 
+	INNER JOIN customer r ON c.relaterid=r.objid 
+WHERE c.principalid=$P{principalid} 
+ORDER BY r.name 
