51e263c00b33d3a8a0c64686fd6c1c0f2574b8b2
worgflores <worgflores@gmail.com>
2013-10-02 17:31:18 +0800

    updated the rameses-clfc-loan-services

--- server/rameses-clfc-loan-services.jar/sql/borrower.sql
+++ server/rameses-clfc-loan-services.jar/sql/borrower.sql
@@ -2,10 +2,10 @@
 SELECT * FROM borrower WHERE objid=$P{objid} 
 
 [getChildren]
-SELECT * FROM borrower_children WHERE borrowerid=$P{borrowerid} 
+SELECT * FROM borrower_children WHERE parentid=$P{parentid} 
 
 [removeChildren]
-DELETE FROM borrower_children WHERE borrowerid=$P{borrowerid} 
+DELETE FROM borrower_children WHERE parentid=$P{parentid} 
 
 [getEmployments]
 SELECT * FROM employment WHERE refid=$P{refid} 
@@ -20,10 +20,10 @@ SELECT * FROM sourceofincome WHERE refid=$P{refid}
 DELETE FROM sourceofincome WHERE refid=$P{refid} 
 
 [getEducations]
-SELECT * FROM borrower_education WHERE borrowerid=$P{borrowerid} 
+SELECT * FROM borrower_education WHERE parentid=$P{parentid} 
 
 [removeEducations]
-DELETE FROM borrower_education WHERE borrowerid=$P{borrowerid} 
+DELETE FROM borrower_education WHERE parentid=$P{parentid} 
 
 [findParent]
 SELECT * FROM borrower_parent WHERE objid=$P{objid} 
@@ -32,22 +32,22 @@ SELECT * FROM borrower_parent WHERE objid=$P{objid}
 DELETE FROM borrower_parent WHERE objid=$P{objid} 
 
 [getSiblings]
-SELECT * FROM borrower_sibling WHERE borrowerid=$P{borrowerid} 
+SELECT * FROM borrower_sibling WHERE parentid=$P{parentid} 
 
 [removeSiblings]
-DELETE FROM borrower_sibling WHERE borrowerid=$P{borrowerid} 
+DELETE FROM borrower_sibling WHERE parentid=$P{parentid} 
 
 [findBankAcct]
 SELECT * FROM borrower_bankacct 
-WHERE borrowerid=$P{borrowerid} AND type=$P{type} 
+WHERE parentid=$P{parentid} AND type=$P{type} 
 
 [getBankAccts]
 SELECT * FROM borrower_bankacct 
-WHERE borrowerid=$P{borrowerid} AND type=$P{type} 
+WHERE parentid=$P{parentid} AND type=$P{type} 
 
 [removeBankAccts]
 DELETE FROM borrower_bankacct 
-WHERE borrowerid=$P{borrowerid} AND type=$P{type} 
+WHERE parentid=$P{parentid} AND type=$P{type} 
 
 [removeAllBankAccts]
-DELETE FROM borrower_bankacct WHERE borrowerid=$P{borrowerid} 
+DELETE FROM borrower_bankacct WHERE parentid=$P{parentid} 
--- server/rameses-clfc-loan-services.jar/sql/loanapp_search.sql
+++ server/rameses-clfc-loan-services.jar/sql/loanapp_search.sql
@@ -6,3 +6,15 @@ FROM loanapp_search_index si
 	INNER JOIN loanapp l ON s.objid=l.objid 
 WHERE si.searchtext LIKE $P{searchtext} AND s.state=$P{state} 
 ORDER BY si.searchtext 
+
+[getCaptureList]
+SELECT DISTINCT 
+	s.*, l.dtcreated, l.createdby  
+FROM loanapp_search_index si 
+	INNER JOIN loanapp_search s ON si.appid=s.objid 
+	INNER JOIN loanapp l ON s.objid=l.objid 
+WHERE 
+	si.searchtext LIKE $P{searchtext} AND 
+	s.state=$P{state} AND 
+	l.appmode='CAPTURE' 
+ORDER BY si.searchtext 
