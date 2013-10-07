aad6505fd8b2c6b9cbe1a080b4e48fab7d21312c
worgflores <worgflores@gmail.com>
2013-10-07 14:55:08 +0800

    updated the rameses-clfc-datamgmt-services

--- server/rameses-clfc-datamgmt-services.jar/sql/loan_route.sql
+++ server/rameses-clfc-datamgmt-services.jar/sql/loan_route.sql
@@ -2,14 +2,17 @@
 SELECT * FROM loan_route
 
 [getLookupRoutes]
-SELECT * FROM loan_route r
+SELECT * FROM loan_route r 
 WHERE ${filter}
 
 [findByCode]
-SELECT * FROM loan_route WHERE code=$P{code} 
+SELECT * FROM loan_route 
+WHERE code=$P{code} 
 
 [getCollectors]
-SELECT * FROM loan_route_collector WHERE parentid=$P{parentid} ORDER BY user_name 
+SELECT * FROM loan_route_collector 
+WHERE parentid=$P{parentid} 
+ORDER BY user_name 
 
 [removeCollector]
 DELETE FROM loan_route_collector WHERE objid=$P{objid}  
