ccd8e289b2f788fffc6899d5f212a6d2036d659c
worgflores <worgflores@gmail.com>
2013-12-03 18:17:18 +0800

    updated the resource db

---
+++ resource/db/utils/cleanup.sql
@@ -0,0 +1,62 @@
+DELETE FROM clfc2b.batch_payment_detail;
+DELETE FROM clfc2b.batch_payment_cashbreakdown;
+DELETE FROM clfc2b.batch_payment;
+
+DELETE FROM clfc2b.borrower;
+DELETE FROM clfc2b.borrower_bankacct;
+DELETE FROM clfc2b.borrower_children;
+DELETE FROM clfc2b.borrower_education;
+DELETE FROM clfc2b.borrower_parent;
+DELETE FROM clfc2b.borrower_sibling;
+
+DELETE FROM clfc2b.customer;
+DELETE FROM clfc2b.customer_connection;
+DELETE FROM clfc2b.employment;
+
+DELETE FROM clfc2b.entitycontact;
+DELETE FROM clfc2b.entityidcard;
+DELETE FROM clfc2b.entityindividual;
+DELETE FROM clfc2b.entityjuridical;
+DELETE FROM clfc2b.entitymember;
+DELETE FROM clfc2b.entitymultiple;
+DELETE FROM clfc2b.entity;
+
+DELETE FROM clfc2b.holiday_event_calendar;
+
+DELETE FROM clfc2b.loan_exemption;
+DELETE FROM clfc2b.loan_ledger_billing;
+DELETE FROM clfc2b.loan_ledger_billing_detail;
+DELETE FROM clfc2b.loan_ledger_billing_lock;
+DELETE FROM clfc2b.loan_ledger_billing_route;
+
+DELETE FROM clfc2b.loan_ledger_detail;
+DELETE FROM clfc2b.loan_ledger_history;
+DELETE FROM clfc2b.loan_ledger;
+
+DELETE FROM clfc2b.loan_payment;
+DELETE FROM clfc2b.loan_payment_detail;
+
+DELETE FROM clfc2b.loan_payment;
+DELETE FROM clfc2b.loan_payment_detail;
+DELETE FROM clfc2b.loan_product_type;
+
+DELETE FROM clfc2b.loan_route;
+
+DELETE FROM clfc2b.loanapp;
+DELETE FROM clfc2b.loanapp_borrower;
+DELETE FROM clfc2b.loanapp_business;
+DELETE FROM clfc2b.loanapp_capture;
+DELETE FROM clfc2b.loanapp_capture_open;
+DELETE FROM clfc2b.loanapp_collateral_appliance;
+DELETE FROM clfc2b.loanapp_collateral_other;
+DELETE FROM clfc2b.loanapp_collateral_property;
+DELETE FROM clfc2b.loanapp_collateral_vehicle;
+DELETE FROM clfc2b.loanapp_log;
+DELETE FROM clfc2b.loanapp_new;
+DELETE FROM clfc2b.loanapp_otherlending;
+DELETE FROM clfc2b.loanapp_recommendation;
+DELETE FROM clfc2b.loanapp_renew;
+DELETE FROM clfc2b.loanapp_search;
+DELETE FROM clfc2b.loanapp_search_index;
+
+DELETE FROM clfc2b.loanapp;
