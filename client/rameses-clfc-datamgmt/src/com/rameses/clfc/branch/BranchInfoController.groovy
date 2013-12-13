2f4b06805a1501933363f5233671aa027c3d814e
worgflores <worgflores@gmail.com>
2013-12-13 14:31:33 +0800

    updated the rameses-clfc-datamgmt

---
+++ client/rameses-clfc-datamgmt/src/com/rameses/clfc/branch/BranchInfoController.groovy
@@ -0,0 +1,37 @@
+package com.rameses.clfc.branch;
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+        
+public class BranchInfoController extends CRUDController 
+{
+    def node, filetype;
+
+    String preferredTitle = 'Branch Information';    
+    String serviceName = 'OrgAdminService';
+    String entityName  = 'BRANCH'; 
+    
+    String createFocusComponent = 'entity.objid';
+    String editFocusComponent = 'entity.name';            
+    boolean showConfirmOnSave = false;
+    boolean allowApprove = false;
+    
+    Map createPermission = [domain:'DATAMGMT', role:'DATAMGMT_AUTHOR', permission:'branch.create']; 
+    Map editPermission = [domain:'DATAMGMT', role:'DATAMGMT_AUTHOR', permission:'branch.edit']; 
+    Map deletePermission = [domain:'DATAMGMT', role:'DATAMGMT_AUTHOR', permission:'branch.delete']; 
+    Map approvePermission = [domain:'DATAMGMT', role:'DATAMGMT_AUTHOR', permission:'branch.approve'];     
+    
+    Map createEntity() {
+        return [
+            orgclass:       'BRANCH', 
+            parentid:       node?.objid, 
+            parentclass:    node?.orgclass 
+        ];
+    } 
+
+    void afterCreate(data) {
+        data.objid = null; 
+    }
+}
