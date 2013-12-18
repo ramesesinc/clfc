a9dcb8162c18d93b82a09f9c7860c7b874069895
Carl Louie Pernito <carlpernito@gmail.com>
2013-12-18 10:27:09 +0800

    added clfc-patch 20131217-1533 and clfc-patch 20131218-1026

---
+++ resource/db/patches/clfc-patch 20131217-1533.sql
@@ -0,0 +1,20 @@
+USE clfc2;
+
+DROP TABLE IF EXISTS `loan_ledger_compromise`;
+CREATE TABLE `loan_ledger_compromise` (
+  `objid` VARCHAR(40) NOT NULL,
+  `compromisetype` VARCHAR(40) DEFAULT NULL,
+  `iswaveinterest` SMALLINT(6) DEFAULT NULL,
+  `iswavepenalty` SMALLINT(6) DEFAULT NULL,
+  `isfixedamount` SMALLINT(6) DEFAULT NULL,
+  `fixedamount` DECIMAL(10,2) DEFAULT NULL,
+  `dtcreated` DATETIME DEFAULT NULL,
+  `createdby` VARCHAR(100) DEFAULT NULL,
+  `dteffectivefrom` DATE DEFAULT NULL,
+  `dteffectiveto` DATE DEFAULT NULL,
+  PRIMARY KEY  (`objid`),
+  KEY `ix_compromisetypeid` (`compromisetype_objid`),
+  KEY `ix_iswaveinterest` (`iswaveinterest`),
+  KEY `ix_iswavepenalty` (`iswavepenalty`),
+  KEY `ix_fixedamount` (`isfixedamount`)
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
---
+++ resource/db/patches/clfc-patch 20131218-1026.sql
@@ -0,0 +1,4 @@
+USE clfc2;
+
+ALTER TABLE batch_collectionsheet
+ADD branchid VARCHAR(10) DEFAULT '';
