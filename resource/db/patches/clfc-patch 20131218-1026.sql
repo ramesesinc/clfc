a0720f389b507dbf97465e133aaaad0d67d0c100
Carl Louie Pernito <carlpernito@gmail.com>
2013-12-18 12:40:02 +0800

    added clfc-patch 20131218-1026

--- resource/db/patches/clfc-patch 20131218-1026.sql
+++ resource/db/patches/clfc-patch 20131218-1026.sql
@@ -1,4 +1,12 @@
 USE clfc2;
 
-ALTER TABLE batch_collectionsheet
-ADD branchid VARCHAR(10) DEFAULT '';
+ALTER TABLE `batch_collectionsheet`
+ADD `branchid` VARCHAR(10) DEFAULT '';
+
+ALTER TABLE `loan_ledger_compromise`
+DROP `iswavepenalty`,
+ADD `iswaivepenalty` SMALLINT(6),
+ADD INDEX `ix_iswaivepenalty` (`iswaivepenalty`),
+DROP `iswaveinterest`,
+ADD `iswaiveinterest` SMALLINT(6),
+ADD INDEX `ix_iswaiveinterest` (`iswaiveinterest`);
