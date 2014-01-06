8ef26d40ee43879ab570ebf199ccc6c040653078
Carl Louie Pernito <carlpernito@gmail.com>
2014-01-06 17:43:53 +0800

    updated rameses clfc loan services

--- server/rameses-clfc-loan-services.jar/sql/loan_ledger_specialcollection.sql
+++ server/rameses-clfc-loan-services.jar/sql/loan_ledger_specialcollection.sql
@@ -10,16 +10,20 @@ SELECT ll.*, sqry.code AS route_code, sqry.area AS route_area,
 FROM loan_ledger ll
 INNER JOIN loanapp la ON ll.appid=la.objid
 INNER JOIN customer c ON ll.acctid=c.objid
-INNER JOIN (SELECT lr.*
+INNER JOIN (SELECT lr.*, sqry.*
 	FROM loan_route lr
-	LEFT OUTER JOIN (SELECT llbr.routecode
+	LEFT OUTER JOIN (SELECT sqry.routecode
 		FROM loan_ledger_billing llb
 		LEFT JOIN loan_ledger_subbilling llsb ON llb.objid=llsb.objid
-		INNER JOIN loan_ledger_billing_route llbr ON llb.objid=llbr.billingid
+		INNER JOIN (SELECT llbr.* 
+			FROM loan_ledger_billing_route llbr
+			LEFT JOIN loan_ledger_specialcollection llsc ON llbr.billingid=llsc.billingid
+			WHERE llsc.objid IS NULL) sqry ON llb.objid=sqry.billingid
 		WHERE llb.collector_objid=$P{collectorid}
 			AND llb.billdate=$P{billdate}) sqry ON lr.code=sqry.routecode
 	WHERE sqry.routecode IS NULL) sqry ON la.route_code=sqry.code
 WHERE ll.state = 'OPEN'
+	AND ll.acctname LIKE '%'
 
 [getLedgersByBillingid]
 SELECT ll.*, lr.code AS route_code, lr.area AS route_area,
