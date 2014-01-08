5d4729f3d89c3a4ac49cc4d5ca5021607f17d4ec
worgflores <worgflores@gmail.com>
2014-01-08 11:21:00 +0800

    updated the rameses-clfc-mobile-services

--- server/rameses-clfc-mobile-services.jar/sql/mobile_tracker.sql
+++ server/rameses-clfc-mobile-services.jar/sql/mobile_tracker.sql
@@ -1,11 +1,8 @@
 [getList]
 SELECT * FROM mobile_tracker WHERE sessionid=$P{sessionid} ORDER BY txndate 
 
-[findBySession]
-SELECT * FROM mobile_tracker WHERE sessionid=$P{sessionid} 
-
-[findBranch]
-SELECT * FROM branch WHERE objid=$P{objid} 
-
 [getLogs]
 SELECT * FROM mobile_tracker_detail WHERE parentid=$P{parentid} ORDER BY txndate 
+
+[findByPrimary]
+SELECT * FROM mobile_tracker WHERE objid=$P{objid} 
