b0ada5acb8f88631b6788e58b32b6118d126073d
worgflores <worgflores@gmail.com>
2014-02-27 13:37:42 +0800

    updated the rameses-clfc-mobile-services

--- server/rameses-clfc-mobile-services.jar/sql/test_mobile_tracker03.sql
+++ server/rameses-clfc-mobile-services.jar/sql/test_mobile_tracker03.sql
@@ -1,8 +1,10 @@
+[getList]
+SELECT * FROM test_mobile_tracker 
+WHERE sessionid=$P{sessionid} ${filter} 
+ORDER BY txndate 
+
 [log]
 INSERT INTO test_mobile_tracker 
 	(objid, txndate, sessionid, lng, lat, accuracy) 
 VALUES 
 	($P{objid}, $P{txndate}, $P{sessionid}, $P{lng}, $P{lat}, $P{accuracy}) 
-
-[getList]
-SELECT * FROM test_mobile_tracker WHERE sessionid=$P{sessionid} ORDER BY txndate 
